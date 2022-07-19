f01d00cae3f2f37518d3667ad4c8fe8d66b91589
jamshidev404 <jamshidbek536@gmail.com>
2022-07-20 00:37:54 +0500

    db

--- src/services/Database.ts
+++ src/services/Database.ts
@@ -4,8 +4,8 @@ import { MONGO_URI } from "../config";
 export default async () => {
   try {
     await mongoose.connect(MONGO_URI, {
-      //useNewUrlParser: true,
-      //useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
 
     console.log("DB connected");
